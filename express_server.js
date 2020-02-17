6ef60c1bea34dfbe337a5ce031cb210af780e285
Hok Lun Siu (Marco) <marco698698@hotmail.com>
2020-02-17 20:07:53 +0000

    fixing the getting the link in /url/:shortURL

--- express_server.js
+++ express_server.js
@@ -36,7 +36,6 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
-
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
