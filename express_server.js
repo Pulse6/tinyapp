401411cac3be4b575673b45ae0f8de08ad060f51
Hok Lun Siu (Marco) <marco698698@hotmail.com>
2020-02-17 22:04:26 +0000

    made generateRandomString for making a unique code

--- express_server.js
+++ express_server.js
@@ -4,10 +4,24 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+   var result           = '';
+   var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      for ( var i = 0; i < 7; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+   }
+   return result;
+}
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -19,11 +33,6 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
 });
 
 app.get("/urls", (req, res) => {
@@ -38,4 +47,19 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console .longURL
+  res.send("Ok");
+  // let uniqueCode = generateRandomString()
+  // let newObj = [generateRandomString(), ]
+  // urlDatabase.push(req.body.longURL)
+  // res.send([generateRandomString(), req.body.longURL]);        // Respond with 'Ok' (we will replace this)
+  res.redirect('/urls')
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
