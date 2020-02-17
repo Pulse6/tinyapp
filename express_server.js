1635fc6c053e8a3c946725adbfdd916b1c5af23c
Hok Lun Siu (Marco) <marco698698@hotmail.com>
2020-02-17 18:35:42 +0000

    made ruls_show.ejs and added new tempatet also got the path to work

--- express_server.js
+++ express_server.js
@@ -30,3 +30,8 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
