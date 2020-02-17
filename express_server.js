f57b4ff075cd0d21623f01e64758f0adf7fb8a2c
Hok Lun Siu (Marco) <marco698698@hotmail.com>
2020-02-17 18:07:43 +0000

    added path (/urls)

--- express_server.js
+++ express_server.js
@@ -25,3 +25,8 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
