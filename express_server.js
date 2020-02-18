23c060a6adaead184e5b87705ddd38042a13870b
Hok Lun Siu (Marco) <marco698698@hotmail.com>
2020-02-18 21:46:42 +0000

    feature cookies works with login and logout

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+let cookieParser = require('cookie-parser');
 const app = express();
-const PORT = 8080; // default port 8080
-
+app.use(cookieParser());
 app.set("view engine", "ejs");
+
+const PORT = 8080; // default port 8080
 
 const generateRandomString = function() {
   let result           = '';
@@ -12,7 +14,6 @@ const generateRandomString = function() {
   }
   return result;
 };
-
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -36,16 +37,19 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase , username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] , username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 
@@ -65,16 +69,20 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect(`/urls`);
 });
 
-// app.post("/urls/:id", (req, res) => {
-//   let id = req.params.id
-//   // urlDatabase[req.params.id] = req.body.update
-//   res.redirect(`/urls/${id}`);
-// });
-
 app.post("/urls/:id/update", (req, res) => {
   let id = req.params.id;
   urlDatabase[req.params.id] = req.body.update;
   res.redirect(`/urls/${id}`);
+});
+
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username', req.body.username);
+  res.redirect('/urls');
 });
 
 
