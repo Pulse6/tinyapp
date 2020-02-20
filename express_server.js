c5f71efc877221d343e95af4bcdf1c9a7c8c35b2
Hok Lun Siu (Marco) <marco698698@hotmail.com>
2020-02-20 14:39:30 +0000

    updated new database and everything that pass thought/ poeple that doesn't login can still use /u/:is

--- express_server.js
+++ express_server.js
@@ -151,7 +151,7 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  const longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);
 });
 
